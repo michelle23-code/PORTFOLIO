d3afc696ec914fa2c84283706f408f6336d5b69f
michelle23-code <michellelekoto13@icloud.com>
2025-01-03 15:55:58 +0200

    added  js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,20 @@
 tailwind.config = {
-  
+    theme: {
+        extend:{
+        gridTemplateColumns:{
+            'auto': 'repeat(auto-fill, minmax(200px, 1fr))',
+        },
+        fontFamily: {
+            Outfit: ["Outfit", "sans-serif"],
+            Ovo: ["Ovo", "sans-serif"],
+        },
+        animation:{
+             spin_slow: 'spin 6s linear infinite',
+        },
+        colors: {
+            lightHover: '#fcf4ff',
+            darkHover: '#2a204a',
+        }
+    }
+}
 }
